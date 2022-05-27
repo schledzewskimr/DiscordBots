3bbfc20ffdab11d447288f6b7d6382caafa3c6f7
Mark Schledzewski <mark.schledzewski@gmail.com>
2022-05-27 14:57:23 -0400

    update

--- main.js
+++ main.js
@@ -7,8 +7,16 @@ client.on("ready", () => {
 })
 
 client.on("message", msg => {
-    if (msg.content.endsWith("?") && msg.content.length>1){
+    if (msg.content.includes("?") && msg.content.length>1){
         msg.reply("great question " + msg.author.username + "!")
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const searchTopic = args.join('+').slice(-1)
+        let googleResult = `https://google.com/search?q=${searchTopic}`
+
+        let searchEmbed = new Discord.MessageEmbed()
+            .setColor("#00ff00")
+            .setDescription(`Here's what Google came up with for ${searchTopic}!\n${googleResult}`)
+        msg.reply.send(searchEmbed);
     }
     if (msg.content.includes("lol")){
         msg.react("😂")
