acc1d049481860fa0f41f672dccaeadf4becf641
Mark Schledzewski <mark.schledzewski@gmail.com>
2022-05-27 17:29:52 -0400

    update

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
 require('dotenv').config();
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const channel = client.channels.cache.get('id');
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on("message", msg => {
+client.on("messageCreate", msg => {
     if (msg.content.endsWith("?") && msg.content.length>1){
-        msg.reply("great question " + msg.author.username + "!")
         const args = msg.content.split(/ +/);
         let searchTopic = args.join('+').slice(0,-1);
         for(let i = 0; i<searchTopic.length; i++){
@@ -17,13 +17,21 @@ client.on("message", msg => {
             }
         }
         let googleResult = `https://google.com/search?q=${searchTopic}`
-        msg.reply('I did a quick google search for "' + msg.content.slice(0,-1) + '" and I found ' + googleResult)
+
+        const embed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle("Google results for: " + msg.content.slice(0,-1))
+            .setURL(googleResult)
+            .setAuthor({ name: msg.author.username, iconURL: 'https://static.wikia.nocookie.net/parody/images/5/5b/Profile_-_Donkey.jpg/revision/latest?cb=20200804211128', url: googleResult })
+            .setDescription(msg.author.username + ' had a question.')
+            .setThumbnail('https://static.wikia.nocookie.net/parody/images/5/5b/Profile_-_Donkey.jpg/revision/latest?cb=20200804211128')
+            msg.channel.send({embeds: [embed]});
     }
     if (msg.content.includes("lol")){
         msg.react("😂")
     }
     if (msg.content.startsWith("setupVote")){
-        const reactions = ["🎃","🤖","👽","👾","👻"]
+        const reactions = ["🎃","🤖","👽","👾","👻","💎","🦍","🚀","🍔"]
         let options = msg.content.slice(msg.content.length-1);
         for(let i = 0; i < options; i++){
             msg.react(reactions[i]);
