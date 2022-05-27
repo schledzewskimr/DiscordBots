3d53879fc9313b8c639c2ebff22c13a639e78cdb
Mark Schledzewski <mark.schledzewski@gmail.com>
2022-05-27 16:04:03 -0400

    update

--- main.js
+++ main.js
@@ -7,16 +7,17 @@ client.on("ready", () => {
 })
 
 client.on("message", msg => {
-    if (msg.content.includes("?") && msg.content.length>1){
+    if (msg.content.endsWith("?") && msg.content.length>1){
         msg.reply("great question " + msg.author.username + "!")
-        const args = message.content.slice(prefix.length).split(/ +/);
-        const searchTopic = args.join('+').slice(-1)
+        const args = msg.content.split(/ +/);
+        let searchTopic = args.join('+').slice(0,-1);
+        for(let i = 0; i<searchTopic.length; i++){
+            if(searchTopic.endsWith("?")){
+                searchTopic=searchTopic.slice(0,-1);
+            }
+        }
         let googleResult = `https://google.com/search?q=${searchTopic}`
-
-        let searchEmbed = new Discord.MessageEmbed()
-            .setColor("#00ff00")
-            .setDescription(`Here's what Google came up with for ${searchTopic}!\n${googleResult}`)
-        msg.reply.send(searchEmbed);
+        msg.reply('I did a quick google search for "' + msg.content.slice(0,-1) + '" and I found ' + googleResult)
     }
     if (msg.content.includes("lol")){
         msg.react("😂")
